e6daddc8abbe54a365f2a8ceb32cd1bbbbbf9a67
Mundiaem <machariamundia@gmail.com>
2022-10-26 10:18:11 +0300

    initial commit

---
+++ Movie Infor Service/src/test/java/com/mundiaem/movieinforservice/MovieInforServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mundiaem.movieinforservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieInforServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
